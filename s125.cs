a3fd52df7fc69c7bdb42ed87e10277af6d328e87
kushal <knar301@aucklanduni.ac.nz>
2025-07-25 20:13:15 +1200

    Solution for lc #125

---
+++ s125.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+//Leetcode 125
+//Solved
+
+public bool class Solution125
+{
+	public static void IsPalindrome(string s)
+	{
+		string t = s.ToLower();
+		List<char> characters = t.ToList();
+		for (int i = characters.Count - 1; i >= 0; i--)
+		{
+			if (!Char.IsLetterOrDigit(characters[i]))
+			{
+				characters.RemoveAt(i);
+			}
+		}
+		int left = 0;
+		int right = characters.Count - 1;
+		while (left < right)
+		{
+			if (characters[left] != characters[right])
+			{
+				return false;
+				break;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+
+}
